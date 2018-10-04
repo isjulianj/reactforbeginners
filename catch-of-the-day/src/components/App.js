f2a346667b3b55f411e0ae325c8d003c9d2eee72
Julian Jackson <julian.jackson@jaywing.com>
2018-10-04 13:31:26 +0100

    on video 6

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import Header from "./Header.js";
+import Header from './Header.js';
+import Inventory from './Inventory.js';
+import Order from './Order.js';
 class App extends React.Component {
   render() {
     return (
-
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header />
+          <Header tagline="Fresh Seafood Market" />
         </div>
-        {/* <Inventory />
-        <Order /> */}
+        <Order />
+        <Inventory />
       </div>
-    )
+    );
   }
 }
 
