2f61fe06ae1c543a11b67baf996335c07a79a430
isjulianJaywing <julian.jackson@jaywing.com>
2018-10-04 22:36:19 +0100

    set up the routing

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,25 +1,19 @@
-import React from 'react';
+import React from "react";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <header className="top">
-          <h1>
-            Catch
-            <span className="ofThe">
-              <span className="of">of</span>
-              <span className="the">the</span>
-            </span>
-            Day
-          </h1>
-          <h3 className="tagline">
-            <span>{this.props.tagline}</span>
-          </h3>
-        </header>
-      </React.Fragment>
-    );
-  }
-}
+const Header = ({ tagline }) => (
+  <header className="top">
+    <h1>
+      Catch
+      <span className="ofThe">
+        <span className="of">of</span>
+        <span className="the">the</span>
+      </span>
+      Day
+    </h1>
+    <h3 className="tagline">
+      <span>{tagline}</span>
+    </h3>
+  </header>
+);
 
 export default Header;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,8 +1,6 @@
-import React from 'react';
-import {render} from 'react-dom';
-import StorePicker from './components/StorePicker.js';
-import App from './components/App.js';
+import React from "react";
+import { render } from "react-dom";
+import Router from "./components/Router";
 import "./css/style.css";
 
-
-render(<App/>, document.getElementById('main'));
+render(<Router />, document.getElementById("main"));
