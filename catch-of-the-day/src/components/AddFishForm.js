f9afefe67a1f810cc6e563c2350dca74bbe443cd
isjulianJaywing <julian.jackson@jaywing.com>
2018-10-12 22:36:42 +0100

    start of fish form

---
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+class AddFishForm extends React.Component {
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  descRef = React.createRef();
+  imageRef = React.createRef();
+  createFish = e => {
+    e.preventDefault();
+     const fish {
+         name : this.nameRef.current.value,
+         prcice: this.priceRef.current.value
+     }  
+  };
+
+  render() {
+    return (
+      <form className="fish-edit" action="" onSubmit={this.createFish}>
+        <input name="name" ref={this.nameRef} type="text" placeholder="Name" />
+        <input
+          name="price"
+          ref={this.priceRef}
+          type="text"
+          placeholder="Price"
+        />
+        <select name="Status">
+          <option value="available"> Fresh</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textarea
+          name="desc"
+          ref={this.descRef}
+          type="text"
+          placeholder="Desc"
+        />
+        <input
+          name="image"
+          ref={this.imageRef}
+          type="text"
+          placeholder="Image"
+        />
+        <button type="submit">Add Fish +</button>
+      </form>
+    );
+  }
+}
+
+export default AddFishForm;
